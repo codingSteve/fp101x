706840c45ef5a939c577effd37b01c5cb1e9a268
Steven Powell <goFasterSteve@gmail.com>
2015-11-07 11:52:37 +0000

    add exercise one examples

--- 20151109/exercise1.hs
+++ 20151109/exercise1.hs
@@ -1,7 +1,43 @@
 
+l  = [] 
+
+l2 = null
+
+l3 = [1,2,3,4,5,6,7,8,9,0]
+
 
 {--
+ -
  - Which of the following implementations are valid for a function safetail :: [a] -> [a] that behaves as the library function tail , except that safetail maps the empty list to itself, whereas tail produces an error in this case. Choose all correct implementations!
  --}
 
-safetail xs = if null xs then [] else tail xs
+safetail0 xs = if null xs then [] else tail xs
+
+safetail1 []     = []
+safetail1 (_:xs) = xs
+
+-- bug here doing tail twice effectively
+safetail2 (_:xs) | null xs   = []
+                 | otherwise = tail xs
+
+safetail3 xs     | null xs   = []
+                 | otherwise = tail xs
+
+-- types are fine here but the order is off
+-- we'll never match the second def
+safetail4 xs = tail xs
+safetail4 [] = []
+
+-- fixes above bug
+safetail5 [] = []
+safetail5 xs = tail xs
+
+-- not safe and not tail
+safetail6 [x]    = [x]
+safetail6 (_:xs) = xs
+
+safetail7 = \ xs -> case xs of []     -> []
+                               (_:xs) -> xs
+
+
+
